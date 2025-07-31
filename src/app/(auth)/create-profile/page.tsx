bb21c054594ea7e477f8782c3e9c707076f4f7ee
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-07-31 09:34:48 +0800

    oage

---
+++ src/app/(auth)/create-profile/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./components/Container";
+import { Header } from "./components/Header";
+
+const Page = () => {
+  return (
+    <div>
+      <Header />
+      <Container />
+    </div>
+  );
+};
+export default Page;
