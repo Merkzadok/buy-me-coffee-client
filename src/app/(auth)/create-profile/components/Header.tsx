2d71ba09d89faaba8872da12422b0119535841c4
25LP8349 25LP8349 <25LP8349@admins-iMac.local>
2025-07-31 09:12:41 +0800

    create page

--- src/app/(auth)/create-profile/components/Header.tsx
+++ src/app/(auth)/create-profile/components/Header.tsx
@@ -2,7 +2,7 @@ import { Coffee } from "lucide-react";
 
 export const Header = () => {
   return (
-    <div className="w-[1440px] h-[56px]">
+    <div className="w-[1440px] h-[56px] mx-auto">
       <div className="flex pt-4 gap-4 place-content-between items-center w-[1280px] h-[40px]">
         <div className="flex mx-[83px]">
           <Coffee />
