f948b34d12f80465da3c7c0aa38091a8b6bac938
25LP1749 25LP1749 <merkzadok@gmail.com>
2025-08-01 09:31:24 +0800

    explore-toolbar

--- src/app/(main)/explore/page.tsx
+++ src/app/(main)/explore/page.tsx
@@ -1,14 +1,18 @@
 import { Header } from "@/components/Header";
 import { ExploreCard } from "./components/ExploreCard";
+import Toolbar from "../home/components/Toolbar";
 
 export default function Explore() {
   return (
     <div>
       <Header />
-      <div className="justify-center flex items-center h-screen w-screen mx-auto flex-col gap-5 ">
-        {Array.from({ length: 5 }).map((_, i) => (
-          <ExploreCard key={i} />
-        ))}
+      <div className="max-w-[1280px] mx-auto w-full flex gap-20 ">
+        <Toolbar />
+        <div className="flex items-center h-screen w-screen mx-auto flex-col  ">
+          {Array.from({ length: 5 }).map((_, i) => (
+            <ExploreCard key={i} />
+          ))}
+        </div>
       </div>
     </div>
   );
